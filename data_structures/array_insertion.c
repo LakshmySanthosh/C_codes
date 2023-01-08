a36b1fabeff9ca1059599223c0245a312f21fb1c
Lakshmy <121610033+LakshmySanthosh@users.noreply.github.com>
2023-01-08 13:59:23 +0530

    Add files via upload
    Program to take an array as input, traverse it, print it, and insert the desired element at the desired position according to user input.

---
+++ data_structures/array_insertion.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+int main()
+{
+    int a[50],i,n,p,k;
+    printf("Enter the  number of elements of the array: ");
+    scanf("%d",&n);
+    printf("Enter the  elements: \n");
+    for (i=1;i<=n;i++)
+        scanf("%d",&a[i]);
+    printf("The array is: \n");
+    for (i=1;i<=n;i++)
+        printf("%d ",a[i]);
+    printf("\nEnter the position in which you want to insert: ");
+    scanf("%d",&p);
+    printf("Enter the element you want to insert: ");
+    scanf("%d",&k);
+    for (i=n;i>=p;i--)
+        a[i+1]=a[i];
+    a[p]=k;
+    n=n+1;
+    printf("The array after insertion is: \n");
+    for (i=1;i<=n;i++)
+        printf("%d ",a[i]);
+}
