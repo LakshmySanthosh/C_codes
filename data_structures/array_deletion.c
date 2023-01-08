d8946643be780b6a3e81f28917b916a1144acac0
Lakshmy <121610033+LakshmySanthosh@users.noreply.github.com>
2023-01-08 14:00:20 +0530

    Add files via upload
    Program to take an array as input, traverse it, print the array and then delete the desired element as per the input from the user.

---
+++ data_structures/array_deletion.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main()
+{
+    int a[50],i,n,p;
+    printf("Enter the number of elements of the array: ");
+    scanf("%d",&n);
+    printf("Enter the elements: \n");
+    for(i=1;i<=n;i++)
+        scanf("%d",&a[i]);
+    printf("The array is:\n");
+    for(i=1;i<=n;i++)
+        printf("%d ",a[i]);
+    printf("\nEnter the position of element you want to delete: ");
+    scanf("%d",&p);
+    for(i=p;i<=n;i++)
+        a[i]=a[i+1];
+    n=n-1;
+    printf("The array after deletion is:\n");
+    for(i=1;i<=n;i++)
+        printf("%d ",a[i]);
+}
