4a2718fae9e898a4fd539524b89ca7fb2ea32a3c
Lakshmy <121610033+LakshmySanthosh@users.noreply.github.com>
2023-01-17 23:50:26 +0530

    Add files via upload
    binary search

---
+++ data_structures/array_binary_search.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+    int n=0,a[n],i,l,h,m,e;
+    while(l<=h)
+        m=(l+h)/2;
+        if(a[m]==e)
+            printf("The element is found ")
+            l=a[m]-1;
+        else(a[m]<e)
+            h=a[m]+1;
+        
+        
+
+
+}
