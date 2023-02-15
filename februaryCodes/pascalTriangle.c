0ca149326fe7779f87d57916c480186250739504
Lakshmy <121610033+LakshmySanthosh@users.noreply.github.com>
2023-02-15 07:43:09 +0530

    Add files via upload
    Pascal Triangle

--- februaryCodes/pascalTriangle.c
+++ februaryCodes/pascalTriangle.c
@@ -1,6 +1,26 @@
 //program to generate and plot the Pascal triangle
-#include <stdio.h>
+#include<stdio.h>
+int fact(int n)
+{
+    int i,fact=1;
+    for(i=1;i<=n;i++)
+        fact=fact*i;
+    return fact;
+}
 int main()
 {
-    
+    int i,j,n,nCr;
+    printf("Enter the numer of rows: ");
+    scanf("%d",&n);
+    for(i=0;i<=n;i++)
+    {
+        for(j=0;j<=n-i;j++)
+            printf(" ");
+        for(j=0;j<=i;j++)
+        {
+            nCr=fact(i)/(fact(j)*fact(i-j));
+            printf("%3d",nCr);
+        }
+        printf("\n");
+    }
 }
